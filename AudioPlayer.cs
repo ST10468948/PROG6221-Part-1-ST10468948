51c92bd405bdee2cf12a9905e3fddf3a32fb16dc
SEDZANI <ST10468948@rcconnect.edu.za>
2026-04-12 20:33:22 +0200

    Audio player Added

--- AudioPlayer.cs
+++ AudioPlayer.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace chatBot
 {
+    // Creating another class called AudioPlayer to handle sound
     public class AudioPlayer
     {
+        // A method that takes a file path string to play music
+        public static void PlayMusic(string filepath)
+        {
+            // Making a new SoundPlayer object so we can use its functions
+            SoundPlayer musicPlayer = new SoundPlayer();
+            // Telling the player where the music file is located on the computer
+            musicPlayer.SoundLocation = filepath;
+            // This actually starts playing the sound
+            musicPlayer.Play();
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,14 +3,23 @@ using System;
 using System.IO;
 using System.Media;
 
+
 namespace PlayMusicC
 {
+    // This is the main class where the program starts
     class Program
     {
 
+        // This is the Main method which is the entry point of the application
         static void Main(string[] args)
         {
+            // Call the PlayMusic method from the AudioPlayer class and give it the file name
+            AudioPlayer.PlayMusic("sage.wav");
+
+            // Call the UserWelcome method from the User class to show the welcome screen
             User.UserWelcome();
+
+            // Call the Chat method from the Chatbot class to start the conversation
             Chatbot.Chat();
 
         }
